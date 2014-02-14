6ff3f50c064b19fd1035b0d9447f833b7011841f
Luis Doubrava <luis@cg.nl>
2014-02-14 10:46:21 +0100

    replace file store with Rails.cache

--- lib/cg_trustpilot/client.rb
+++ lib/cg_trustpilot/client.rb
@@ -4,21 +4,22 @@ module CgTrustpilot
     # http library for doing a http request
     require "net/http"
 
-    def parse_trustpilot_feed(filename = 'feed.json', feed_url = nil)
-      file = "#{CgTrustpilot.config[:temp_folder]}/#{filename}"
+    def parse_trustpilot_feed(name = 'feed.json', feed_url = nil)
+      # file = "#{CgTrustpilot.config[:temp_folder]}/#{name}"
       # json parse the unzipped tempfile and return as a Trustpilot feed object
 
-      get_trustpilot_feed(filename, feed_url) unless File.exists?(file)
+      trustpilot_rating = Rails.cache.fetch(name, race_condition_ttl: 2.minutes) do
+        get_trustpilot_feed(name, feed_url)
+      end
 
-      file_contents = File.open(file) { |f| f.read }
-      CgTrustpilot::Feed.new(JSON.parse(file_contents))
+      CgTrustpilot::Feed.new(JSON.parse(trustpilot_rating))
     rescue
       CgTrustpilot::Feed.new({})
     end
 
-    def get_trustpilot_feed(filename = 'feed.json', feed_url = nil)
+    def get_trustpilot_feed(name = 'feed.json', feed_url = nil)
       hash = Securandom.base64
-      file = "#{CgTrustpilot.config[:temp_folder]}/#{hash}.#{filename}"
+      # file = "#{CgTrustpilot.config[:temp_folder]}/#{hash}.#{name}"
       # get the feed url from the config
       feed_url ||= CgTrustpilot.config[:feed]
 
@@ -28,14 +29,15 @@ module CgTrustpilot
       res = http.get(uri.request_uri)
       case res
         when Net::HTTPSuccess
+          Rails.cache.write(name, res.body.force_encoding("utf-8"), expires_in: 15.minutes)
           # save the result as file
-          File.open(file,'w') do |f|
-            f.write res.body.force_encoding("utf-8")
-          end
-          File.rename(file, "#{CgTrustpilot.config[:temp_folder]}/#{filename}")
+          # File.open(file,'w') do |f|
+#             f.write res.body.force_encoding("utf-8")
+#           end
+#           File.rename(file, "#{CgTrustpilot.config[:temp_folder]}/#{name}")
       end
     ensure
-      File.unlink(file)
+      # File.unlink(file)
     end
   end
 end
--- lib/cg_trustpilot/controller.rb
+++ lib/cg_trustpilot/controller.rb
@@ -4,7 +4,7 @@ require 'cg_trustpilot/feed'
 module CgTrustpilot
   module Controller
 
-    def trustpilot(filename = 'feed.json', feed_url = nil)
+    def trustpilot(name = 'feed.json', feed_url = nil)
       client = CgTrustpilot::Client.new
       render :partial => "trustpilot/block", :locals => {:feed => client.parse_trustpilot_feed}
     end
